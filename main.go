bc8ea20e687308b435925efaabaf6d709a01f1f4
inomoto <medalhkr@gmail.com>
2023-02-11 19:57:42 +0900

    almost ok

--- main.go
+++ main.go
@@ -2,33 +2,61 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
+	"io"
+	"log"
+	"net/url"
+	"os"
+	"os/signal"
 	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 func main() {
-	ctx := context.TODO()
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "Usage: cat file | s3put s3://your-bucket-name/path/to/dest")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
 
+	u, err := url.ParseRequestURI(flag.Arg(0))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	bucket := u.Host
+	key := strings.TrimPrefix(u.Path, "/")
+	body := io.MultiReader(os.Stdin)
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	if err = uploadObject(ctx, bucket, key, body); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to put object; %v", err)
+	}
+}
+
+func uploadObject(ctx context.Context, bucket, key string, body io.Reader) error {
 	conf, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
-		fmt.Println("Couldn't load default configuration. Have you set up your AWS account?")
-		fmt.Println(err)
-		return
+		return err
 	}
 
+	// MEMO: To upload unknown size data, use Uploader instead of client.PutObject
+	// https://stackoverflow.com/questions/43595911/how-to-save-data-streams-in-s3-aws-sdk-go-example-not-working
 	client := s3.NewFromConfig(conf)
-	bucket := "some-bucket"
-	key := "hoge"
-	client.PutObject(ctx, &s3.PutObjectInput{
+	uploader := manager.NewUploader(client)
+	_, err = uploader.Upload(ctx, &s3.PutObjectInput{
 		Bucket: &bucket,
 		Key:    &key,
-		Body:   strings.NewReader("fuga"),
+		Body:   body,
 	})
 	if err != nil {
-		fmt.Printf("err: %v\n", err)
-		return
+		return err
 	}
+
+	return nil
 }
